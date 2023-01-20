701335afd0c29fba22aae72a6f547ef305a9f6bb
DjDarkz <zd819@ic.ac.uk>
2023-01-20 16:26:07 +0000

    Sidebar Open/Close

--- Extension samples/extension-getting-started-part2/extension-getting-started-part2/popup/display.js
+++ Extension samples/extension-getting-started-part2/extension-getting-started-part2/popup/display.js
@@ -1,24 +1,69 @@
 // const nameVisible = document.getElementById("name2");
-// nameVisible.innerHTML = "Zion";
+// nameVisible.addEventListener("mouseover", over());
 
+var sidebaropen = false
 
 function showElement() {
     element = document.querySelector('.container');
     element.style.visibility = 'visible';
 }
 
-function hideElement() {
-    element = document.getElementById('SettingsIcon');
+function click() {
+    // console.log("Hide Element");
+    var element = document.getElementById('name2');
+    // element.innerHTML += "Click!<br>";
+    ToggleSideBar();
+    // element.style.visibility = 'hidden';
+}
+
+function over() {
+    // console.log("Hide Element");
+    var element = document.getElementById('name2');
+    element.innerHTML += "Over!<br>";
+    // element.style.visibility = 'hidden';
+}
+
+function ToggleSideBar() {
+    console.log("Show SideBar");
+    var element = document.getElementById('TopSidebar');
+    switch(element.style.visibility) {
+        case "visible":
+          // code block
+          console.log("Hidden now");
+          element.style.visibility = "hidden";
+          break;
+        case "hidden":
+          // code block
+          console.log("Visible now");
+          element.style.visibility = "visible";
+          break;
+        default:
+            console.log(element.style.visibility)
+            element.style.visibility = "hidden";
+          // code block
+      }
+       
+}
+
+function hide() {
+    console.log("Hide SideBar");
+    var element = document.getElementById('TopSidebar');
     element.style.visibility = 'hidden';
 }
 
 
-var btn = document.getElementById("Settings Icon");
+document.addEventListener("onpageshow", hide()); 
 
-btn.addEventListener("click", alertMe);
+// var btn = document.getElementById("Settings Icon");
+// btn.addEventListener("click", alertMe);
 
-function alertMe(){
-    element = document.getElementById('SettingsIcon');
-    element.style.visibility = 'hidden';
+// function alertMe(){
+//     element = document.getElementById('SettingsIcon');
+//     element.style.visibility = 'hidden';
 
-}
+// }
+
+
+var SettingsIcon = document.getElementById("SettingsIcon");
+SettingsIcon.addEventListener("click", click);
+// SettingsIcon.addEventListener("mouseover", over);
